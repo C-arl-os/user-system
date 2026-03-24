1c70d5a80e323a51d2f9e00e3184b1d2667c202e
C-arl-os <3636514110101@ingenieria.usac.edu.gt>
2026-03-23 23:50:54 -0600

    feat: conectar base de datos PostgresSQL

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 require('dotenv').config();
 const express = require('express')
 const userRoutes = require('./src/routes/user.routes');
-
+const pool = require('./src/config/database');
 
 const app = express();
 const PORT = process.env.PORT;
+
+pool.connect()
+    .then(() => console.log('Conexión a la base de datos exitosa'))
+    .catch(err => console.error('Error al conectar a la base de datos', err.message));
 
 app.use(express.json());
 app.use('/api/users', userRoutes);
